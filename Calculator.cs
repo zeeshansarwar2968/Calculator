4ec343dcfd35ac3e66bd483aa26860fecca6060c
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-02 09:02:11 +0530

    Added calculator functionality

---
+++ Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public void Add()
+        {
+            int num1 = 20;
+            int num2 = 40;
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of two numbers is {0}",sum);
+        }
+
+        public void Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of two numbers {0}+{1}={2}",num1,num2, sum);
+        }
+
+        public void Subtraction(int num1, int num2)
+        {
+            int subtraction = num1 - num2;
+            Console.WriteLine("Subtraction of two numbers {0}-{1}={2}", num1, num2, subtraction);
+        }
+
+        public void Multiplication(int num1, int num2)
+        {
+            int multiplication = num1 * num2;
+            Console.WriteLine("multiplication of two numbers {0}*{1}={2}", num1, num2, multiplication);
+        }
+
+        public void Division(int num1, int num2)
+        {
+            int division = num1 / num2;
+            Console.WriteLine("Division of two numbers {0}/{1}={2}", num1, num2, division);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calcObj = new Calculator();
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            calcObj.Add();
+            calcObj.Addition(num1, num2);
+        }
+    }
+}
+
